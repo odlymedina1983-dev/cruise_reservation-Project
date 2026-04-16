3098af5667ccbefce07b81c2bf8b0dccee3e2d97
odlymedina1983-dev <odly.medina1983@gmail.com>
2026-04-15 23:57:27 -0400

    feat: add CTA component with variants, states and responsive icon system

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import MainLayout from './layouts/MainLayout'
 import Container from './components/Container'
+import CTA from './components/CTA/CTA'
 
 function App() {
 return (
@@ -9,6 +10,27 @@ return (
                 <h1>Cruiser Project-Fantasm</h1>
                 <h2>Cruiser Project-Century</h2>
                 <h3>Cruiser Project-Inter</h3>
+                <CTA
+                  label="Book now"
+                  state="default"
+                  size="mobile"
+                  type="primary"
+                  icon="none"
+                />
+                <CTA
+                  label="Book now"
+                  state="default"
+                  size="desktop"
+                  type="primary"
+                  icon="left"
+                />
+                <CTA
+                  label="Book now"
+                  state="default"
+                  size="tablet"
+                  type="secondary"
+                  icon="right"
+                />
               </Container>    
         </main>
     </MainLayout>
